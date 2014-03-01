a5031ac1d4f6b49203c1d26afe5def0c289c933e
Tobias Tikka <tobias.tikka@gmail.com>
2014-03-01 11:33:09 +0100

    ListGraph - done

--- graphs/ListGraph.java
+++ graphs/ListGraph.java
@@ -17,27 +17,47 @@ public class ListGraph  extends AbstractGraph{
 		}
 	}
 
+	/**inserts an edge to the list at the source position.
+	 * If the graph isn't directed it will be added to the destination position as well.
+	 *
+	 */
 	public void insert(Edge edge) {
 		edges[edge.getSource()].add(edge);
 		if(!isDirected())
+			// Make a new edge so it doesn't refer to the same object as we just inserted to the list.
 			edges[edge.getDest()].add(new Edge(edge.getSource(), edge.getDest(), edge.getWeight()));		
 	}
 
-	@Override
+	/**Checks if and Edge exists in the list.
+	 * 
+	 */
 	public boolean isEdge(int source, int dest) {
-		// TODO Auto-generated method stub
-		return false;
+				return edges[source].contains(new Edge(source, dest));
 	}
 
-	@Override
+	/**Get the edge between two vertices. If an
+	 * edge does not exist, an Edge with a weight
+	 * of Double.POSITIVE_INFINITY is returned.
+	 * @param	source	the source
+	 * @param	dest	the destination
+	 * @return	the edge between these two verices.
+	 * 
+	 */
 	public Edge getEdge(int source, int dest) {
-		// TODO Auto-generated method stub
-		return null;
+		Edge target = new Edge(source, dest, Double.POSITIVE_INFINITY);
+		for(Edge edge : edges[source]){
+			if(edge.equals(target))
+				return edge;	//both the source and destination was the same.
+		}
+		return target;
+			
 	}
 
-	@Override
+	/**Returns an iterator for one of the ArrayLists in the List.
+	 * 
+	 */
+	
 	public Iterator<Edge> edgeIterator(int source) {
-		// TODO Auto-generated method stub
-		return null;
+		return edges[source].iterator();
 	}
 }
